ca8b0fdc23e0bdddb74a4399452c54698f256e64
pooya <pooya13vm@gmail.com>
2022-11-13 11:37:10 +0300

    icon sat

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,15 +1,74 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Alert, ScrollView, View, Text, Image } from "react-native";
+import { Alert } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Input, CheckBox, Button } from "@rneui/themed";
 import Icon from "react-native-vector-icons/Ionicons";
-import { AppContext } from "../context/context";
+import styled from "styled-components";
+
 //personal components
 import BackGround from "../components/BackGround";
-//personal tools
+import { AppContext } from "../context/context";
 import { COLORS } from "../tools/colors";
 import CircleButton from "../components/CircleButton";
 
+//styled components
+const Container = styled.View`
+  background-color: #fefefe;
+  height: 75%;
+  width: 90%;
+  align-self: center;
+  margin-top: 35%;
+  border-radius: 30px;
+  align-items: center;
+`;
+const ImageComponent = styled.Image`
+  width: 120px;
+  height: 120px;
+  margin-top: -60px;
+  background-color: #fefefe;
+  border-radius: 70px;
+`;
+const BodyContainer = styled.ScrollView`
+  width: 90%;
+`;
+const SexSelectContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+const CheckboxContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  border-color: gray;
+  padding: 20px;
+  border-radius: 15px;
+`;
+const ButtonContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+const SPContainer = styled.View`
+  align-items: center;
+  margin-top: 10%;
+`;
+const SPimageComponent = styled.Image`
+  width: 220px;
+  height: 220px;
+  margin-vertical: 10%;
+`;
+const SPTitleText = styled.Text`
+  font-size: 38px;
+  font-weight: bold;
+  color: ${COLORS.darkBlue};
+  margin-top: 15%;
+`;
+const SPNameText = styled.Text`
+  font-size: 24px;
+  margin-top: 20px;
+  color: ${COLORS.darkBlue};
+  border-bottom-width: 2px;
+  border-bottom-color: ${COLORS.darkBlue};
+  padding: 10px;
+`;
 const inputsStyle = {
   backgroundColor: "transparent",
   borderColor: COLORS.darkBlue,
@@ -21,53 +80,37 @@ const inputsStyle = {
 
 const Register = ({ navigation }) => {
   const [name, setName] = useState("");
-  const [alias, setAlias] = useState("");
+  const [serialNum, setSerialNum] = useState("");
+  const [sportGN, setSportGn] = useState("");
   const [checkedMan, setCheckedMan] = useState(false);
   const [checkedWoman, setCheckedWoman] = useState(false);
 
   const { setContact, contact, checkStorage } = useContext(AppContext);
 
   const saveContact = () => {
-    if (alias === "") {
-      Alert.alert("Lütfen boşluğu doldurun 'Kullanıcı adı'");
+    if (name === "" && serialNum === "" && sportGN === "") {
+      Alert.alert("Lütfen hep boşluğu doldurun");
     } else {
-      setContact([{ name, alias, sex: checkedMan ? "male" : "female" }]);
+      setContact([{ name, serialNum, sex: checkedMan ? "male" : "female" }]);
       navigation.navigate("Home");
     }
   };
 
   useEffect(() => {
-    if (!contact.alias) {
+    if (!contact.name) {
       checkStorage();
     }
   }, []);
-
-  if (!contact.alias) {
+  console.log(contact);
+  if (!contact.name) {
     return (
       <KeyboardAwareScrollView>
         <BackGround>
-          <View
-            style={{
-              backgroundColor: "#fefefe",
-              height: "75%",
-              width: "90%",
-              alignSelf: "center",
-              marginTop: "35%",
-              borderRadius: 30,
-              alignItems: "center",
-            }}
-          >
-            <Image
+          <Container>
+            <ImageComponent
               source={require("../assets/images/Screen_Shot_2022-10-27_at_13.56.36-removebg.png")}
-              style={{
-                width: 120,
-                height: 120,
-                marginTop: -60,
-                backgroundColor: "#fefefe",
-                borderRadius: 70,
-              }}
             />
-            <ScrollView style={{ width: "90%" }}>
+            <BodyContainer>
               <Input
                 label="Cihazın seri numarası : "
                 style={inputsStyle}
@@ -79,7 +122,7 @@ const Register = ({ navigation }) => {
                 }}
                 containerStyle={{ height: 90 }}
                 labelStyle={{ marginBottom: 10, color: COLORS.darkBlue }}
-                onChangeText={(val) => setName(val)}
+                onChangeText={(val) => setSerialNum(val)}
               />
               <Input
                 label="Spor kompleksinin adı :"
@@ -95,7 +138,7 @@ const Register = ({ navigation }) => {
                   marginBottom: 10,
                   color: COLORS.darkBlue,
                 }}
-                onChangeText={(val) => setAlias(val)}
+                onChangeText={(val) => setSportGn(val)}
               />
               <Input
                 label="Kullanıcı adı : *"
@@ -107,26 +150,11 @@ const Register = ({ navigation }) => {
                   color: COLORS.darkGreen,
                 }}
                 labelStyle={{ marginBottom: 10, color: COLORS.darkBlue }}
-                onChangeText={(val) => setAlias(val)}
+                onChangeText={(val) => setName(val)}
               />
 
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-around",
-                  // marginVertical: 20,
-                }}
-              >
-                <View
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderColor: "gray",
-                    // borderWidth: 1,
-                    padding: 20,
-                    borderRadius: 15,
-                  }}
-                >
+              <SexSelectContainer>
+                <CheckboxContainer>
                   <Icon
                     name="man"
                     type="ionicons"
@@ -145,17 +173,8 @@ const Register = ({ navigation }) => {
                       if (checkedWoman === true) setCheckedWoman(false);
                     }}
                   />
-                </View>
-                <View
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderColor: "gray",
-                    // borderWidth: 1,
-                    padding: 20,
-                    borderRadius: 15,
-                  }}
-                >
+                </CheckboxContainer>
+                <CheckboxContainer>
                   <Icon
                     name="woman-sharp"
                     type="ionicons"
@@ -174,58 +193,30 @@ const Register = ({ navigation }) => {
                       if (checkedMan === true) setCheckedMan(false);
                     }}
                   />
-                </View>
-              </View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  width: "100%",
-                  justifyContent: "space-evenly",
-                  alignItems: "center",
-                  // marginTop: 20,
-                }}
-              ></View>
-              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                </CheckboxContainer>
+              </SexSelectContainer>
+              <ButtonContainer>
                 <CircleButton
                   title="üye Ol"
                   onPressFunction={() => saveContact()}
                 />
-              </View>
-            </ScrollView>
-          </View>
+              </ButtonContainer>
+            </BodyContainer>
+          </Container>
         </BackGround>
       </KeyboardAwareScrollView>
     );
   } else {
     return (
       <BackGround>
-        <View style={{ alignItems: "center", marginTop: "10%" }}>
-          <Image
-            style={{ width: 220, height: 220, marginVertical: "10%" }}
+        <SPContainer>
+          <SPimageComponent
             source={require("../assets/images/Screen_Shot_2022-10-27_at_13.56.36-removebg.png")}
           />
-          <Text
-            style={{
-              fontSize: 38,
-              fontWeight: "bold",
-              color: COLORS.darkBlue,
-              marginTop: "15%",
-            }}
-          >
-            Hoşgeldiniz
-          </Text>
-          <Text
-            style={{
-              fontSize: 24,
-              marginTop: 20,
-              color: COLORS.darkBlue,
-              borderBottomWidth: 2,
-              borderBottomColor: COLORS.darkBlue,
-              padding: 10,
-            }}
-          >{`${contact.name} ${
-            contact.sex === "male" ? "Bey" : "Hanım"
-          }`}</Text>
+          <SPTitleText>Hoşgeldiniz</SPTitleText>
+          <SPNameText>{`${contact.name} ${
+            contact.sex === "female" ? "Hanım" : "Bey"
+          }`}</SPNameText>
 
           <Button
             title="Giriş yapmak"
@@ -239,7 +230,7 @@ const Register = ({ navigation }) => {
             titleStyle={{ color: COLORS.darkBlue, fontSize: 18 }}
             onPress={() => navigation.navigate("Home")}
           />
-        </View>
+        </SPContainer>
       </BackGround>
     );
   }
